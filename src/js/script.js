6424dca30f63addd8b87b2944768ed7e26ff27c4
Kelwin <kelwin.sbastos21@gmail.com>
2025-04-01 09:10:10 -0300

    Declaração de Variaveis

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+// DECLARAÇÃO E VARIAVEIS
+var nome = "FIAP"; //DECLARA UMA VARIAVEL "VAR" QUE RECEBE O NOME "FIAP".
+console.log(nome); //EXIBE NO CONSOLE O QUE VOCÊ DECLARAR.
+
+// -------------------------------------------
+let idade = 17;
+console.log(idade);
+
+// -------------------------------------------
+const sobreNome ="Silva";
+console.log(sobreNome);
+
+//undefined
+let aula;
+console.log(aula)
+
+//null
+let valor = null;
+console.log(valor);
+
+//Objeto com coisas especificas
+let exemplo1={};
+
+//Objeto array
+let exemplo2=[];
